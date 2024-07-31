cfff739207104470ea386890b988d3e6605281bb
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-07-31 15:12:59 +0000

    update

---
+++ miner.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RByQMmvQRRYEPPW3pXBAqyDj1BGhk1gaPK --Worker VerusPC1 --password Hybrid --cpu-threads 0
